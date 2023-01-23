f212f3e495b7a678febac5a626dc6691ab69bcdc
shravani <116992213+shravanimerugu7@users.noreply.github.com>
2023-01-23 11:07:13 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer();
+server.on("request", (req, res)   => {
+    fs.readFile("index.html" , (err , data)=>{
+        res.end(data)
+    });
+});
+
+server.listen(5000, () => console.log("Server Started"));
